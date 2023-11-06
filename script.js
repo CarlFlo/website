1fbbfc8a4d5cd997e8a1aa394378b9535b930fab
CarlFlo <carl.flodin@hotmail.com>
2023-11-06 22:06:14 +0100

    updated js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/*
 (function () {
     'use strict';
 
@@ -6,8 +5,8 @@
     var year = new Date().getFullYear();
     document.querySelector('footer p').innerHTML += year;
 
+    /*
     // Page view counter
-
     var namespace = "068ac0d2-bee3-4e21-a930-0e615521c67f"
     var key = "cf0f82ad-8274-4a56-bf61-9c5eba359b60"
 
@@ -18,6 +17,6 @@
         document.getElementById("vCount").innerText += ` ${this.response.value}`
     }
     xhr.send();
+    */
 
 }());
-*/
